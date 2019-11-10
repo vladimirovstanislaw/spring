140ee27e7ddbbc5b1d21d7ce774b0739a6e8c053
stas <zstanislaw211@gmail.com>
2019-11-10 15:54:03 +0300

    vv

--- src/main/java/test/mysql/Runable.java
+++ src/main/java/test/mysql/Runable.java
@@ -1,8 +1,56 @@
 package test.mysql;
 
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.GregorianCalendar;
+import java.util.List;
+
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+import jdbc.Contact;
+import jdbc.ContactTelDetail;
+import spring.jdbc.ContactDao;
+
 public class Runable {
 	public static void main(String[] args) {
 
+		GenericXmlApplicationContext ctx = new GenericXmlApplicationContext();
+		ctx.load("classpath:META-INF/spring/mysql-context.xml");
+		ctx.refresh();
+		ContactDao contactDao = ctx.getBean("contactDao", ContactDao.class);
+
+		Contact contact = new Contact();
+		contact.setFirstName("Michael");
+		contact.setLastName("Jackson");
+		contact.setBirthDate(new Date((new GregorianCalendar(1964, 10, 1)).getTime().getTime()));
+
+		List<ContactTelDetail> contactTelDetails = new ArrayList<>();
+		ContactTelDetail contactTelDetail = new ContactTelDetail();
+		contactTelDetail.setTelType("Home");
+		contactTelDetail.setTelNumber("11111111");  
+		System.out.println();
+		contactTelDetails.add(contactTelDetail);
+
+		contactTelDetail = new ContactTelDetail();
+		contactTelDetail.setTelType("Mobile");
+		contactTelDetail.setTelNumber("22222222");
+		contactTelDetails.add(contactTelDetail);
+		contact.setContactTelDetails(contactTelDetails);
+
+		contactDao.insertWithDetail(contact);
+
+		listContacts(contactDao.findAll());
 	}
 
+	public static void listContacts(List<Contact> contacts) {
+		for (Contact contact : contacts) {
+			System.out.println(contact);
+			if (contact.getContactTelDetails() != null) {
+				for (ContactTelDetail contactTelDetail : contact.getContactTelDetails()) {
+					System.out.println("---" + contactTelDetail);
+				}
+			}
+			System.out.println();
+		}
+	}
 }
