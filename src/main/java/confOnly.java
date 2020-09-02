5454127ddd2722467510bdd53c1b35cc9075b4ce
stas <zstanislaw211@gmail.com>
2020-09-03 00:49:15 +0300

    Before

--- src/main/java/confOnly.java
+++ src/main/java/confOnly.java
@@ -14,9 +14,9 @@ git add src/main/java/confOnly.java
 git add target/classes/META-INF/maven/spring/spring.me/pom.properties
 
 
-#cd /c/Users/svladimirov/eclipse-workspace/spring/spring
+#cd C:/Users/zstan/projects/spring/spring
 git add .
-git commit -m "4th chapter started, yuhooo"
+git commit -m "Before"
 #git remote add origin git@github.com:vladimirovstanislaw/spring.git
 git push -u origin master
 ##git push --force -u origin master
